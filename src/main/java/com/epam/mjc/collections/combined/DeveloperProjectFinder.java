cda120a53c739f3844f7a130db8324cbb328210f
Vahe <vahe.jaloyan34@gmail.com>
2023-10-25 22:44:20 +0400

    ready

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        for (String key :
+                projects.keySet()) {
+            if (projects.get(key).contains(developer)) {
+                result.add(key);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,26 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+        for (String key :
+                sourceMap.keySet()) {
+            if(result.containsKey(key.length())){
+                Set<String> tmp = result.get(key.length());
+                tmp.add(key);
+                result.put(key.length(), tmp);
+            }else{
+                Set<String> tmp = new HashSet<>();
+                tmp.add(key);
+                result.put(key.length(), tmp);
+            }
+
+        }
+        return result;
     }
 }
